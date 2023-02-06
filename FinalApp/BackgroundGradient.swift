bed5143e1c790cf098d054ae561592d87aa71257
Daksh <daksh2998@gmail.com>
2023-02-06 17:02:06 +0530

    Initial Commit

---
+++ FinalApp/BackgroundGradient.swift
@@ -0,0 +1,46 @@
+//
+//  BackgroundGradient.swift
+//  FinalApp
+//
+//  Created by Daksh on 02/02/23.
+//
+
+import UIKit
+
+class BackgroundGradient: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        customUI()
+    }
+    func customUI(){
+        print("In Custom View")
+        setGradientBackground(colorTop: UIColor(named: "Dark")!, colorBottom: UIColor(named: "Light")!)
+        
+    }
+    
+    func setGradientBackground(colorTop: UIColor, colorBottom: UIColor) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [colorTop.cgColor, colorBottom.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
+        gradientLayer.locations = [0.3, 1]
+        gradientLayer.frame = view.bounds
+        view.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FinalApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinalApp
+//
+//  Created by Daksh on 02/02/23.
+//
+
+import UIKit
+
+class ViewController: BackgroundGradient {
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
